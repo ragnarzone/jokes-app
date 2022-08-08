5e308fb84767006d0eb66fbe75b2ed08048d46d4
ragnarzone <ragnarzone@gmail.com>
2022-08-08 15:27:26 +0300

    added working joke app

---
+++ src/main/java/com/ragnarzone/jokesapp/controllers/JokesController.java
@@ -0,0 +1,21 @@
+package com.ragnarzone.jokesapp.controllers;
+
+import com.ragnarzone.jokesapp.services.JokeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokesController {
+    private final JokeService jokeService;
+
+    public JokesController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model) {
+        model.addAttribute("joke", jokeService.getJoke());
+        return "index";
+    }
+}
---
+++ src/main/java/com/ragnarzone/jokesapp/services/JokeService.java
@@ -0,0 +1,5 @@
+package com.ragnarzone.jokesapp.services;
+
+public interface JokeService {
+    String getJoke();
+}
